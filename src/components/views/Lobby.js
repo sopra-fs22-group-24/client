103e5994686e159eda8105628ecbee40c7b4ecff
Lea Kehrli <lea_sophia_zaugg@hotmail.com>
2022-04-21 09:28:42 +0200

    Lobby

--- src/components/views/Lobby.js
+++ src/components/views/Lobby.js
@@ -1,72 +1,98 @@
 import BaseContainer from "components/ui/BaseContainer";
-//import { api } from "helpers/api";
-import { Button } from 'components/ui/Button';
-//import React, { useState } from 'react';
+import {useHistory} from 'react-router-dom';
+//import { Lobby } from 'components/ui/Lobby';
 import React, {useEffect, useState} from 'react';
 import {api, handleError} from 'helpers/api';
-import SocketConnection from 'helpers/socketConnection';
-import "styles/views/Profile.scss";
+//import SocketConnection from 'helpers/socketConnection';
+import {Circle} from "../ui/Circle";
+import {LobbyButton} from "../ui/LobbyButton";
+import "styles/views/Lobby.scss";
 
 
 const Lobby = () => {
     const [lobbies, setLobbies] = useState(null);
+    
+    const history = useHistory();
     /* var socket = new SocketConnection();
     socket.subscribe("/lobby/messages", callBackFunction);
     socket.connect(localStorage.getItem('token')); */
-    //const response = api.get("/lobby", {headers:{Authorization:localStorage.getItem('token')}})
-    
-    // the effect hook can be used to react to change in your component.
-    // in this case, the effect hook is only run once, the first time the component is mounted
-    // this can be achieved by leaving the second argument an empty array.
-    // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
+
+    //fetch all lobby-data
     useEffect(() => {
     // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-    async function fetchData() {
-      try {
-        const response = await api.get("/lobby", {headers:{Authorization:localStorage.getItem('token')}});
+      async function fetchData() {
+        try {
+          const response = await api.get("/lobby", {headers:{Authorization:localStorage.getItem('token')}});
 
-        // Get the returned lobbies and update the state.
-        setLobbies(response.data);
-        
-      } catch (error) {
-        console.error(`Something went wrong while fetching the lobbies: \n${handleError(error)}`);
-        console.error("Details:", error);
-        alert("Something went wrong while fetching the users! See the console for details.");
+          // Get the returned lobbies and update the state.
+          setLobbies(response.data);
+          
+        } catch (error) {
+          console.error(`Something went wrong while fetching the lobbies: \n${handleError(error)}`);
+          console.error("Details:", error);
+          alert("Something went wrong while fetching the users! See the console for details.");
+        }
       }
-    }
+      fetchData();}, []
+    );
+  console.log(lobbies);
 
-    fetchData();
-    }, []);
+  
+  
+  // lobby-content displayed correctly   
   let content; 
   if (lobbies) {
     content = (
-      <div className="lobby">
-        <ul className="lobby lobbyList">
-            {lobbies.map(lobby => ( 
-                <div>LobbyId: {lobby.lobbyId} </div>        
-             
+       <div className="lobby">
+          <ul className="lobby lobbyList"> 
+            {lobbies.map(lobby => (
+                <div>
+                  <LobbyButton>
+                    <div>Game {lobby.lobbyId}</div>
+                    <div>Players joined:</div>
+                    {lobby.players.map(player => (
+                      <div>{player.username}</div>
+                    ))}
+                    {/* <div>{lobby.players.forEach(player => (
+                      <div>{lobby.players.length} Players joined: {player.username}
+                    </div>))}</div> */}
+                    
+                     
+                  </LobbyButton> 
+                </div> 
               ))}         
 
-        </ul>  
-      </div>
+          </ul>  
+       </div>
     );
   } 
+  
+  
+
+  function goToProfile() {
+    history.push('/profile');
+  }
     
-    return (
-        <BaseContainer className="profile container">
+  return (
+      <BaseContainer>
+        <div className="lobby ownUser">
+          <Circle
+              onClick={() => goToProfile()}
+          >
+              ME
+          </Circle>    
+        </div>
+        <div className="lobby container">
+          <div className="lobby form">
             <h2>Lobby</h2>
-            {/* <h1>'response.data(1)'</h1>
-            <p className="lobby paragraph">
-            Get all lobbies from secure endpoint:
-            </p>
-            {content} */}
+            
             {content}
-            <Button
-                onClick={() => console.log(lobbies)}></Button>
-        </BaseContainer>
-        
-    );
+          </div>
+        </div>
+      </BaseContainer>
+      
+  );
 }
 
 
-export default Lobby;
+export default Lobby; 
