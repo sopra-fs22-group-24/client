f510830830dd4e6e0a29127b2568877491e561d1
livzel <liviaegli@hotmail.com>
2022-05-25 16:49:34 +0200

    updated some rules and desing rules page

--- src/components/views/Rules.js
+++ src/components/views/Rules.js
@@ -13,8 +13,8 @@ const objectText = "The first player to play all of the cards in their hand wins
 const setUpText = "Each player is dealt 7 cards. One card is placed in the middle of the game"
 const specialCardText = "Reverse Card:This card reverses direction of play. Skip Card: The next person in line to play after this card is played loses his/her turn and is skipped. This card may only be played on matching color or on another Skip card. Hit 2: The next person must hit the launcher twice and forfeit his/her turn. This card may only be played on a matching color or on another Hit 2 card. Discard All: Play this card when you want to discard all of the cards in your hand of the same color. For example, if a yellow 7 is in the Discard pile, you may discard all the yellow cards in your hand, with a yellow Discard All card placed on top. Wild Card: This card can be played on any card. When you play this card, you may change the color being played to any color. Extreme Hit:  When this card is played, whoever played it gets to call any color and then choose any other player in the game to hit."
 const letsPlayText = "Match the top card on the discard pile either by number, color or word. For example, if the card is a Green 7, you must play a green card or any color 7. Or, you may play any Wild card. If you don't have anything that matches, you must press the launcher. If you draw a card you can play, play it. Otherwise, play moves to the next person."
-const unoText = "Before playing your next to last card, you must click on the UNO Button. If you don't press it and another player catches you with just one card before the next player begins their turn you must press the launcher twice. If you are not caught before the next player either plays or draws a card, you do not have to draw the extra cards. Once the player plays their last card, the hand is over. Points are tallied and you start over again."
-const scoringText = "If you are first to get rid of all your cards, you get points for the cards left in the other players hands.All number cards (0-9): Face Value. Hit 2: 20 Points. Reverse: 20 Points. Skip: 20 Points. Discard All: 30 Points. Wild: 50 Points. Extreme Hit: 50 Points"
+const unoText = "Before playing your next to last card, you must click on the UNO Button. If you don't press it and another player catches you with just one card before the next player begins their turn you must press the launcher twice. If you are not caught before the next player either plays or draws a card, you do not have to draw the extra cards. Once the player plays their last card, the game is over."
+
 
 function synthesizeSpeech(text) {
     const speechConfig = sdk.SpeechConfig.fromSubscription("1cb611117bf54151b4b078fcdff6ee56", "switzerlandnorth");
@@ -62,7 +62,7 @@ const Rules = () => {
             </div>
             <div className="rules container">
                 <div className="rules form">
-                    <h1>Rules</h1>
+                    <h1>Rules...</h1>
                     <h3> Object <Circle className = "audio" onClick = {() => synthesizeSpeech(objectText)}> <GiSpeaker/> </Circle></h3>
                     <p> {objectText} </p>
                     <h3>Set up<Circle className = "audio" onClick = {() => synthesizeSpeech(setUpText)}> <GiSpeaker/> </Circle></h3>
@@ -84,17 +84,6 @@ const Rules = () => {
                     <p> {letsPlayText} </p>
                     <h3> Uno<Circle className = "audio" onClick = {() => synthesizeSpeech(unoText)}> <GiSpeaker/> </Circle></h3>
                     <p> {unoText} </p>
-                    <h3> Scoring<Circle className = "audio" onClick = {() => synthesizeSpeech(scoringText)}> <GiSpeaker/> </Circle></h3>
-                    <p> If you are first to get rid of all your cards, you get points for the cards left in the other
-                        players hands.
-                    </p>
-                    <p> All number cards (0-9): Face Value</p>
-                    <p> Hit 2: 20 Points</p>
-                    <p> Reverse: 20 Points</p>
-                    <p> Skip: 20 Points</p>
-                    <p> Discard All: 30 Points</p>
-                    <p> Wild: 50 Points</p>
-                    <p> Extreme Hit: 50 Points</p>
                     <Button
                         width="7em"
                         onClick={() => goToDashboard()}
