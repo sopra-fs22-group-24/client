7196897c7381f8e4a277497545d7286c5590bc6b
Mauro Hirt <mauro.hirt@uzh.ch>
2022-05-27 15:17:08 +0200

    Display Profile Images in Game #102

--- src/components/ui/Enemy.js
+++ src/components/ui/Enemy.js
@@ -1,18 +1,44 @@
 import "styles/ui/Enemy.scss";
-import {GoPerson} from "react-icons/go";
+import "styles/ui/picture.scss"
+import React, {useEffect, useState} from 'react';
+import {api, handleError} from 'helpers/api';
+
+
 
 export const Enemy = (props) => {
     let username = props.username;
+    const [picture, setPicture] = useState(null);
     let nCards = props.nCards;
     let isCurrentTurn = props.isCurrentTurn;
     let isCurrentTurnClass = "game singleEnemy";
+
     isCurrentTurnClass = isCurrentTurn ? isCurrentTurnClass + " activePlayer" : isCurrentTurnClass;
 
+    async function fetchProfilePicture() {
+        try {
+            const response = await api.get(`/users/picture/${username}`);
+           setPicture("data:image/jpeg;base64," + response.data)
+            
+        } catch (error) {
+            alert(`Something went wrong fetching the picture\n${handleError(error)}`);
+        }
+    }
+    useEffect(() => {
+
+        fetchProfilePicture();
+    }, []);
     return(
+    <div>
+     
+        
+       
         <div className ={isCurrentTurnClass}>
-            <h1><GoPerson/></h1>
+        <img className = "picture"
+                altf = "" src={picture}/>
+       
             <p> {username} </p>
             <p> {nCards} cards </p>
         </div>
+    </div>
     )
 }
